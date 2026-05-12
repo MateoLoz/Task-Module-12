4e74c375a54255996b22a0bbde783562f162d671
Mateo <mateobinance1@gmail.com>
2026-05-12 11:11:29 -0300

    fix reporters for jenkins

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -131,7 +131,15 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'spec',
+        ['junit', {
+            outputDir: './test-results/',
+            outputFileFormat: function (options: { cid: any }) {
+                return `results-${options.cid}.xml`
+            }
+        }]
+    ],
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
